a6143ddb535d0b218b1e3c32995963eb3bb94558
Ian List <ian_alexander.list@mailbox.tu-dresden.de>
2017-04-14 01:26:46 +0200

    Swiftly rewrote the party Generator into a class for ease of use

--- party_generator.py
+++ party_generator.py
@@ -5,27 +5,58 @@ MPC = MPDClient()
 MPC.timeout = 10
 MPC.connect('localhost', 6600)
 
-SONG_QUERY = []
-RECENT_SONGS = []
+class Client:
 
-def __init_query():
-    init = MPC.listplaylistinfo('Awesome Music')[:5]
-    for elem in init:
-        song = MPC.find('title', elem['title'])[0]
-        add_song(song, 'localhost')
+    def __init__(self, host='localhost', port=6600):
+        self.client = MPDClient()
+        self.client.timeout = 10
+        self.client.connect(host, port)
+        self.queue = []
+        self.recent = []
+
+    def init_query(self):
+        init = self.client.listplaylistinfo('Awesome Music')[:5]
+        for elem in init:
+            song = self.client.find('title', elem['title'])[0]
+            self.add_song(song, 'localhost')
 
 
 
-def query_song(**query):
-    mpd_compliant_query = []
-    for key in query:
-        mpd_compliant_query.append(key)
-        mpd_compliant_query.append(query.get(key))
-    print(mpd_compliant_query)
-    return MPC.find(*mpd_compliant_query)
+    def query_song(self, **query):
+        mpd_compliant_query = []
+        for key in query:
+            mpd_compliant_query.append(key)
+            mpd_compliant_query.append(query.get(key))
+        print(mpd_compliant_query)
+        return self.client.find(*mpd_compliant_query)
 
-def sort_rankings():
-    SONG_QUERY.sort(key=song_key)
+    def sort_rankings(self):
+        self.queue.sort(key=song_key)
+
+    def add_song(self, song, ip):
+        song_list = [x[0] for x in self.queue]
+        if song in song_list:
+            self.vote_song(song, ip, True)
+        else:
+            self.queue.append((song, time(), set()))
+            self.vote_song(song, ip, True)
+
+    def vote_song(self, song, ip, vote):
+        to_vote = None
+        for elem in self.queue:
+            if song == elem[0]:
+                to_vote = elem
+                break
+        if (ip, True) in to_vote[2] or (ip, False) in to_vote[2]:
+            return False
+        to_vote[2].add((ip, vote))
+        self.sort_rankings()
+        return True
+
+    def mpd_add(self):
+        self.sort_rankings()
+        song = self.queue.pop()[0]
+        MPC.add(song['file'])
 
 def song_key(song):
     song_set = song[2]
@@ -36,34 +67,3 @@ def song_key(song):
         else:
             karma = karma - 1
     return (karma, -1 * song[1])
-
-def add_song(song, ip):
-    song_list = [x[0] for x in SONG_QUERY]
-    if song in song_list:
-        vote_song(song, ip, True)
-    else:
-        SONG_QUERY.append((song, time(), set()))
-        vote_song(song, ip, True)
-
-def vote_song(song, ip, vote):
-    to_vote = None
-    for elem in SONG_QUERY:
-        if song == elem[0]:
-            to_vote = elem
-            break
-    if (ip, True) in to_vote[2] or (ip, False) in to_vote[2]:
-        return False
-    to_vote[2].add((ip, vote))
-    sort_rankings()
-    return True
-
-def mpd_add():
-    sort_rankings()
-    song = SONG_QUERY.pop()[0]
-    MPC.add(song['file'])
-
-__init_query()
-vote_song(SONG_QUERY[3][0], 'spme', True)
-mpd_add()
-for elem in SONG_QUERY:
-    print(elem)
