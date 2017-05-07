06882436e58b48b36c5dd81acb2a9c6aa47d0c83
2xlink <2xlink@web.de>
2017-05-08 00:37:04 +0200

    Button vote adds Karma w/ JS, Button styled

--- src/static/script.js
+++ src/static/script.js
@@ -1,6 +1,17 @@
 function button_vote(obj, song_id, weight) {
 	console.log("Called vote with " + song_id + " and " + weight);
-	obj.style.backgroundColor = "green";
+	if (weight != 1 && weight != -1) {
+		console.log("Weight does not seem to be right ...");
+		return false;
+	}
+
+	obj.style.backgroundColor = obj.style.color;
+
+    var karma_element = document.getElementById('karma_' + song_id);
+    var karma = karma_element.innerHTML;
+    karma = Number(karma) + weight;
+    karma_element.innerHTML = karma;
+
 	$.post( "/vote", { song_id: song_id, weight: weight } );
 	return false;
 };
