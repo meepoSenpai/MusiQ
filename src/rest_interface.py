4519a2970a06251603e118ee5bbb06b25c76eadf
2xlink <2xlink@web.de>
2017-05-12 22:49:19 +0200

    JS WIP

--- src/rest_interface.py
+++ src/rest_interface.py
@@ -3,12 +3,12 @@ from musi_q import Client, calculate_karma
 
 app = Flask(__name__)
 
-CLIENT = Client(host="192.168.178.48", default_playlist="Dank Musics")
+CLIENT = Client(host="192.168.0.15", default_playlist="Dank Musics")
 
 # url_for('static', filename='style.css')
 
-def vote_song(song_id, ip_addr, weight):
-    CLIENT.vote_song(song_id, ip_addr, int(weight))
+# def vote_song(song_id, ip_addr, weight):
+#     return CLIENT.vote_song(song_id, ip_addr, int(weight))
 
 def search_song(term):
     return term
@@ -17,25 +17,25 @@ def get_song_list():
     # Magic begins here
     return [elem for elem in CLIENT.queue][::-1]
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
     error = None
-    if request.method == 'POST':
-        return "test"
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
     return render_template('index.html',
         ERROR=error,
         SONGS=get_song_list(),
         F_KARMA=calculate_karma)
 
+@app.route('/getKarma', methods=['GET'])
+def getKarma():
+    song_id = request.args.get("song_id", "")
+    return calculate_karma(song_id)
+
 @app.route('/vote', methods=['POST'])
 def vote():
-    CLIENT.vote_song(
-        request.form['song_id'], 
-        request.environ['REMOTE_ADDR'], 
-        int(request.form['weight']))
-    return "You will never see this"
+    return str(CLIENT.vote_song(
+            request.form['song_id'], 
+            request.environ['REMOTE_ADDR'], 
+            int(request.form['weight'])))
 
 @app.route('/search')
 def search():
--- src/static/script.js
+++ src/static/script.js
@@ -1,17 +1,26 @@
 function button_vote(obj, song_id, weight) {
-	console.log("Called vote with " + song_id + " and " + weight);
-	if (weight != 1 && weight != -1) {
-		console.log("Weight does not seem to be right ...");
-		return false;
-	}
+    console.log("Called vote with " + song_id + " and " + weight);
+    if (weight != 1 && weight != -1) {
+        console.log("Weight does not seem to be right ...");
+        return false;
+    }
 
-	obj.style.backgroundColor = obj.style.color;
+    $.ajax({
+        type: "POST",
+        url: "/vote",
+        data: { song_id: song_id, weight: weight },
+        success: function(response) {
+            console.log("Got a response from the server: " + response)
+            if (response == "True") {
+                if (weight == 1) { obj.style.backgroundColor = "LightGreen"; };
+                if (weight == -1) { obj.style.backgroundColor = "OrangeRed"; };
+                var karma_element = document.getElementById('karma_' + song_id);
 
-    var karma_element = document.getElementById('karma_' + song_id);
-    var karma = karma_element.innerHTML;
-    karma = Number(karma) + weight;
-    karma_element.innerHTML = karma;
-
-	$.post( "/vote", { song_id: song_id, weight: weight } );
-	return false;
+                $.get( "/getKarma", {song_id: song_id} , function( data ) {
+                    karma_element.innerHTML = data
+                });
+            }
+        }
+    });
+    return true;
 };
