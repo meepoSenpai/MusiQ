cb5f4e3e4c6358937a7c03d3a6eec8be946e8438
2xlink <2xlink@web.de>
2017-05-13 19:31:55 +0200

    IT WORKS!

--- src/rest_interface.py
+++ src/rest_interface.py
@@ -1,9 +1,9 @@
-from flask import request, Flask, render_template
+from flask import request, Flask, render_template, json
 from musi_q import Client, calculate_karma
 
 app = Flask(__name__)
 
-CLIENT = Client(host="localhost", default_playlist="Dank Musics")
+CLIENT = Client(host="192.168.0.15", default_playlist="Dank Musics")
 
 # url_for('static', filename='style.css')
 
@@ -53,8 +53,29 @@ def search():
     if title == "" and artist == "" and album == "":
         return "What am i supposed to do without anything"
     d = {"title": title, "artist": artist, "album": album}
-    print(CLIENT.query_song(**d))
-    return 'Hello'
+    songs = CLIENT.query_song(**d)
+    print(songs)
+    songs = [(x, json.dumps(x)) for x in songs if x.get('file') != None and x.get('file').startswith('spotify:track')]
+    return render_template("searchResults.html",
+        SONGS=songs)
+
+@app.route('/addSong', methods=['POST'])
+def addSong():
+    song = request.form["song"]
+    print(song)
+    print(type(song))
+    song = json.loads("{}".format(song))
+    print(song)
+    CLIENT.add_song(song, request.environ['REMOTE_ADDR'])
+    return "<script>onload(window.location = '/');</script>"
+
+    # if title == "" and artist == "" and album == "":
+    #     return "What am i supposed to do without anything"
+    # d = {"title": title, "artist": artist, "album": album}
+    # songs = CLIENT.query_song(**d)
+    # print(songs)
+    # return render_template("searchResults.html",
+    #     SONGS=songs)
 
 if __name__ == "__main__":
     app.run()
